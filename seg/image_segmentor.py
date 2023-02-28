986e51b6611549790ffc78bec0e845bcc239b2b8
rohiitb <bhikulerohit@gmail.com>
2023-02-28 12:49:02 -0500

    Img seg

---
+++ seg/image_segmentor.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+
+from mmseg.apis import inference_segmentor, init_segmentor, show_result_pyplot
+from mmseg.core.evaluation import get_palette
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('img', help='Image folder')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument(
+        '--device', default='cpu', help='Device used for inference')
+    parser.add_argument(
+        '--palette',
+        default='cityscapes',
+        help='Color palette used for segmentation map')
+    args = parser.parse_args()
+
+    # build the model from a config file and a checkpoint file
+    model = init_segmentor(args.config, args.checkpoint, device=args.device)
+    # test a single image
+    result = inference_segmentor(model, args.img)
+    # show the results
+    show_result_pyplot(model, args.img, result, get_palette(args.palette), bool_write=True, out_path='trial_2.png')
+
+
+if __name__ == '__main__':
+    main()
